1fe42521ae6f0c45b4be80f8b15b38122138275d
Nguyen Quy Quang <quang20hn@gmail.com>
2020-08-28 10:56:27 +0700

    Update service

--- TKLaptopFromReques/src/SearchLaptopFromReques/services/LaptopService.java
+++ TKLaptopFromReques/src/SearchLaptopFromReques/services/LaptopService.java
@@ -1,6 +1,6 @@
 package SearchLaptopFromReques.services;
 
-import SearchLaptopFromReques.models.LaptopEntityClass;
+import SearchLaptopFromReques.models.LaptopEntity;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -10,16 +10,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Service {
+public class LaptopService {
     private Connection conn;
 
-    public Service(Connection conn) {
+    public LaptopService(Connection conn) {
         this.conn = conn;
     }
 
-    public void EntityLaptop(ResultSet resultSet, List<LaptopEntityClass> laptopEntityClasses) throws SQLException {
+    public void EntityLaptop(List<LaptopEntity> laptopEntities, String SqlQuerryToDB) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(SqlQuerryToDB);
+
         while (resultSet.next()) {
-            laptopEntityClasses.add(new LaptopEntityClass(resultSet.getString(2),
+            laptopEntities.add(new LaptopEntity(resultSet.getString(2),
                             resultSet.getString(3),
                             resultSet.getString(4),
                             resultSet.getString(5),
@@ -38,8 +41,8 @@ public class Service {
     }
 
     //Follow price
-    public List<LaptopEntityClass> FindLaptopFromPrice(Float price1, Float price2) {
-        List<LaptopEntityClass> laptopEntityClasses = new ArrayList<LaptopEntityClass>();
+    public List<LaptopEntity> FindLaptopFromPrice(Float price1, Float price2) {
+        List<LaptopEntity> laptopEntities = new ArrayList<LaptopEntity>();
         if (price2 == null && price1 == null) {
             return null;
         }
@@ -48,43 +51,38 @@ public class Service {
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
 
-            EntityLaptop(resultSet, laptopEntityClasses);
+            EntityLaptop(laptopEntities, sql);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return laptopEntityClasses;
+        return laptopEntities;
     }
 
     //Follow Maker and ssd
-    public List<LaptopEntityClass> FindLaptopFromSSD_MAKER(String maker, String ssd) {
-        ArrayList<LaptopEntityClass> laptopEntityClasses = new ArrayList<LaptopEntityClass>();
+    public List<LaptopEntity> FindLaptopFromSSD_MAKER(String maker, String ssd) {
+        ArrayList<LaptopEntity> laptopEntities = new ArrayList<LaptopEntity>();
         if (maker == null && ssd == null) {
             return null;
         }
         String sql1 = "SELECT * FROM store_cms_plusplus.laptop WHERE maker = '" + maker + "' AND ssd = '" + ssd + "';";
         String sql2 = "SELECT * FROM store_cms_plusplus.laptop WHERE maker =  '" + maker + "' OR ssd = '" + ssd + "';";
         try {
-            Statement statement = conn.createStatement();
-            ResultSet resultSet;
             if (maker == null) {
-                resultSet = statement.executeQuery(sql1);
+                EntityLaptop(laptopEntities,sql1);
             } else if (ssd == null) {
-                resultSet = statement.executeQuery(sql1);
+                EntityLaptop(laptopEntities,sql1);
             } else {
-                resultSet = statement.executeQuery(sql2);
+                EntityLaptop(laptopEntities,sql2);
             }
-
-            EntityLaptop(resultSet, laptopEntityClasses);
-
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return laptopEntityClasses;
+        return laptopEntities;
     }
 
     //Follow User From Fontend
-    public List<LaptopEntityClass> FindLaptopFromReques(){
-        List<LaptopEntityClass> laptopList = new ArrayList<>();
+    public List<LaptopEntity> FindLaptopFromReques() {
+        List<LaptopEntity> laptopList = new ArrayList<>();
         String QuerryPrice = "WHERE price BETWEEN 10000000 AND 15000000;"; // or 3tr-10tr or 10tr-15tr or 15tr->
         String QuerryMakerAcer = "WHERE maker = 'acer';"; // or APPLE or HP or ASUS
         String QuerryScreenSize14inch = "WHERE screeen_size = 14"; // or 15,6 or 13
@@ -94,19 +92,28 @@ public class Service {
         String QuerrySort = "ORDER BY price DESC"; // or Ram or Price or ssd or screen_size or sold or screen_resolution
         String QuerryCard = "WHERE card LIKE 'Intel%"; // or NVIDA%( GeForce GTX% - RTX) or AMD%
 
-        String SqlQuerryToDB = "SELECT * FROM store_cms_plusplus.laptop" ;
+        String SqlQuerryToDB = "SELECT * FROM store_cms_plusplus.laptop";
 
         try {
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery(SqlQuerryToDB);
-
-            EntityLaptop(resultSet, laptopList);
+            EntityLaptop(laptopList,SqlQuerryToDB);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return laptopList;
     }
 
+    public List<LaptopEntity> FindLaptopsold() {
+        List<LaptopEntity> laptopList = new ArrayList<>();
+        String SqlQuerryToDB = "SELECT * FROM store_cms_plusplus.laptop ORDER BY sold DESC";
+
+        try {
+            EntityLaptop(laptopList,SqlQuerryToDB);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return laptopList;
+    }
 }
 
 
